cc819ba9323ffa52c2afa033f6b2ac1f7c4c1436
Mathieu Thibeault <2484152@cegepmontpetit.ca>
2026-03-23 18:06:31 -0400

    FCT: Il reste les commentaires et le formulaire

--- question3/src/app/app.component.ts
+++ question3/src/app/app.component.ts
@@ -6,6 +6,9 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { FormGroup } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 
 @Component({
@@ -13,16 +16,23 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
     standalone: true,
-    imports: [MatToolbarModule, MatIconModule, MatCardModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule]
+    imports: [MatToolbarModule, MatIconModule, MatCardModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, CommonModule, RouterModule]
 })
 export class AppComponent {
+  formGroup: FormGroup;
   title = 'reactive.form';
-
   
-
-  constructor(
-    
-  ) { }
+   constructor(private formBuilder: FormBuilder) {
+    this.formGroup = this.formBuilder.group(
+      {
+        name: ['', [Validators.required]],
+        roadnumber: [null, [Validators.required, Validators.min(1000), Validators.max(9999)]],
+        rue: ['', [Validators.required]],
+        postalcode: ['', [Validators.required, Validators.pattern('^[A-Z][0-9][A-Z][ ]?[0-9][A-Z][0-9]$')]],
+        comments: [''],
+      },
+    );
+  }
 }
 
 
