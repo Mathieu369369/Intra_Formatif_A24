74feb19f018541b266d31429ed05aa0fd25e0b41
Mathieu Thibeault <2484152@cegepmontpetit.ca>
2026-03-24 09:28:53 -0400

    FCT: ReactiveForms terminé

--- question3/src/app/app.component.ts
+++ question3/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { AbstractControl, FormBuilder, ValidationErrors, Validators, ReactiveFormsModule } from '@angular/forms';
+import { AbstractControl, FormBuilder, ValidationErrors, Validators, ReactiveFormsModule, ValidatorFn } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -12,27 +12,55 @@ import { RouterModule } from '@angular/router';
 
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css'],
-    standalone: true,
-    imports: [MatToolbarModule, MatIconModule, MatCardModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, CommonModule, RouterModule]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  standalone: true,
+  imports: [MatToolbarModule, MatIconModule, MatCardModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, CommonModule, RouterModule]
 })
 export class AppComponent {
   formGroup: FormGroup;
   title = 'reactive.form';
-  
-   constructor(private formBuilder: FormBuilder) {
+
+  constructor(private formBuilder: FormBuilder) {
     this.formGroup = this.formBuilder.group(
       {
         name: ['', [Validators.required]],
         roadnumber: [null, [Validators.required, Validators.min(1000), Validators.max(9999)]],
         rue: ['', [Validators.required]],
-        postalcode: ['', [Validators.required, Validators.pattern('^[A-Z][0-9][A-Z][ ]?[0-9][A-Z][0-9]$')]],
-        comments: [''],
+        postalcode: ['', [Validators.pattern('^[A-Z][0-9][A-Z][ ]?[0-9][A-Z][0-9]$')]],
+        comments: ['', [mots(10)]],
       },
+      { validators: NomDansCommentaire() }
     );
   }
+
+}
+
+export function mots(minWords: number): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    if (!control.value) return null;
+
+    const words = control.value.trim().split(" ");
+    return words.length >= minWords ? null : { minWords: { required: minWords, actual: words.length } };
+  };
 }
 
 
+
+export function NomDansCommentaire(): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+
+    const comment = control.get('comments')?.value
+    const nomCherche = control.get('name')?.value
+    if (!comment || !nomCherche) {
+      return null
+    }
+    const contientNom = comment.toLowerCase().includes(nomCherche.toLowerCase());
+
+    if (comment.includes(nomCherche)) {
+      return contientNom ? { NomDansCommentaire: true } : null
+    }
+    else return null
+  }
+}
